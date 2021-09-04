c41ea263e6296f28c1cbdd39e05a84022c84a2d7
zuraic <seribupedang@gmail.com>
2021-09-04 23:36:28 +0700

    base code

---
+++ data.py
@@ -0,0 +1,74 @@
+# question_data = [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+#     {"text": "In West Virginia, USA, if you accidentally hit an animal with "
+#              "your car, you are free to take it home to eat.", "answer": "True"},
+#     {"text": "In London, UK, if you happen to die in the House of Parliament, "
+#              "you are entitled to a state funeral.", "answer": "False"},
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+
+question_data = [
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Greenland is almost as big as Africa.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "A group of islands is called an &#039;archipelago&#039;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "Greenland is covered with grass and Iceland covered with ice.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "There is a city called Rome in every continent on Earth.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "Alaska is the largest state in the United States.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "Ottawa is the capital of Canada.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "Tokyo is the capital of Japan.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "New Haven is the capital city of the state of Connecticut in the United States.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "Nova Scotia is on the east coast of Canada.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "There is an island in Japan called \u014ckunoshima, "
+                    "A.K.A. &quot;Rabbit Island&quot;, so named because of "
+                    "it&#039;s huge population of rabbits.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]}
+]
